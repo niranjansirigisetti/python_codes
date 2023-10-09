aae4fee96dfcb9f92d46aa6d444cfbb5f812f93d
niranjansirigisetti <125555941+niranjansirigisetti@users.noreply.github.com>
2023-10-09 19:25:36 +0530

    Add files via upload

---
+++ arithmetic operations.py
@@ -0,0 +1,17 @@
+########arithmetic operations
+a=int(input())
+b=int(input())
+##addition
+print(a+b)
+##subraction
+print(a-b)
+##multiplication
+print(a*b)
+##division
+c=a/b
+print(round(c,2))
+print(c)
+##power
+print(a**b)
+##flowr division
+print(a//b)
